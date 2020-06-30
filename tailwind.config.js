a30d6badd4afcf18654c0f3806cddfa66505f6d9
Nic Goodman <nicgoodman@Nics-MBP.lan>
2020-06-30 11:25:29 -0400

    started initial tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'tag-light-grey': '#F2F2F2',
+        'tag-light-orange': '#FCB76F',
+        'tag-dark-orange': '#F47521',
+        'tag-light-red': '#E25A5F',
+        'tag-red': '#FF1A00',
+        'tag-dark-red': '#BF3026',
+        'tag-yellow': '#FBEE3C',
+        'tag-teal': '#4FBBCD',
+        'tag-dark-teal': '#147F91',
+      }
+    },
+    fontFamily: {
+      'proxima-nova': ['proxima-nova', 'sans-serif'],
+      'quicksand': ['Quicksand', 'sans-serif'],
+    }
   },
-  variants: {},
+  variants: {
+    zIndex: ['responsive', 'hover', 'focus'],
+    textColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+  },
   plugins: [
     require('@tailwindcss/ui'),
   ],
